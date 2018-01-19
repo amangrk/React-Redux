1a9627bf1b85005ae1f6372ddbe4d953437457fa
amangrk <amangrk@gmail.com>
2018-01-19 18:42:44 +1100

    Add files via upload

---
+++ Weather_Predictor/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "5f1ad3f6af41c4271c6f98ea8603e12e";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
