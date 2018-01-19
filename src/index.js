c6826dcf5485ff21a3251e2d7ee8c26fef124a8c
amangrk <amangrk@gmail.com>
2018-01-19 18:24:40 +1100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import _ from 'lodash'
+import Searchbar from './components/search_bar'
+import Videolist from './components/video_list'
+import Videodetail from './components/video_detail'
+import YTSeach from 'youtube-api-search'
+
+const API_Key='AIzaSyA3WN0TDiwxU4h6JtwTvTaItNs2-VFjDjA'
+// const API_Key='0370c5bfeb700b44ded9f24f1818f0bf '
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state={videos : [],
+                selectedVideo : null};
+
+    this.videoSearch('Motivational Videos');
+
+  }
+
+  videoSearch(term)
+  {
+    YTSeach({key : API_Key, term : term },
+            (videos) => {this.setState({videos : videos, selectedVideo : videos[0]});
+          });
+  }
+
+
+  render() {
+    const videoSearch=_.debounce((term) =>{this.videoSearch(term) }, 300);
+
+    return (
+    <div className="body">
+    <Searchbar onSearchChange={videoSearch} />
+    <Videodetail video={this.state.selectedVideo} />
+    <Videolist
+    onVideoSelect={selectedVideo => this.setState({selectedVideo : selectedVideo })}
+    videos={this.state.videos} />
+    </div>
+  );
+  }
+}
+
+ReactDOM.render(<App />,document.querySelector('.container'));
